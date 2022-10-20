d0811e7d6ad425421c91fbddf399fccd7fe3c3b2
ProgrammerJR <103417697+CodingAssassins@users.noreply.github.com>
2022-10-20 18:10:14 +0400

    Add files via upload

--- OBJ_Detection.py
+++ OBJ_Detection.py
@@ -1,7 +1,7 @@
 import cv2
+import pathlib
 import numpy as np
 from tkinter import *
-from tkinter import ttk
 
 
 def obj_detection():
@@ -85,8 +85,35 @@ def obj_detection():
     capture.release()
     output.release()
     cv2.destroyAllWindows()
+    
+def face_recignition():
+    cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+    
+    clf = cv2.CascadeClassifier(str(cascade_path))
+    
+    cam = cv2.VideoCapture(0)
+    
+    while True:
+        _, frame = cam.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = clf.detectMultiScale(
+            gray,
+            scaleFactor = 1.1,
+            minNeighbors = 5,
+            minSize = (30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+        
+        for (x, y, width, height) in faces:
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
+            
+        cv2.imshow("Faces", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cam.release()
+    cam.destroyAllWindows()
 
 
-
-obj_detection()
+face_recignition()
             
