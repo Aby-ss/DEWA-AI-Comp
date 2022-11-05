3b56348cfe84131cc1a37db05b62c91938a6b400
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-11-05 15:23:48 +0400

    Control App

---
+++ Control App/App.py
@@ -0,0 +1,38 @@
+from rich.panel import Panel
+from rich.console import Console
+from rich.traceback import install
+
+from rich import box
+from rich import print
+from rich.layout import Layout
+
+install(show_locals = True)
+
+layout = Layout()
+console = Console()
+
+class Header:
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "[b]LOGIC GATE[/b] control centre",
+            datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="white on blue")
+    
+layout.split_column(
+    Layout(name = "header"),
+    Layout(name = "body"),
+    Layout(name = "footer")
+)
+
+layout.split_row(
+    Layout(name = "upper"),
+    Layout(name = "lower")
+)
+
+layout["header"].update(Header())
+
+print(layout)
