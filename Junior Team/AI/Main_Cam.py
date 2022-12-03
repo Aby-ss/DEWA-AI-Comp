6746c2fe223aa7ec8426de09168f66c6b944a46d
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-03 13:46:14 +0400

    Fixed Issue in Cam reading #2

--- Junior Team/AI/Main_Cam.py
+++ Junior Team/AI/Main_Cam.py
@@ -4,12 +4,15 @@ import cv2
 from rich.traceback import install
 install(show_locals=True)
 
-img = cv2.imread("Junior Team\\Pictures\\image.jpg", 1)
+img = cv2.imread("C:\\Users\\hadir\\Documents\\VSC - Projects\\DEWA-AI-Comp\\Junior Team\\Pictures\\image.jpg", 1)
 
 blur = cv2.GaussianBlur(img, (7, 7), 2)
 h, w = img.shape[:2]
 
 # Binarize gradient
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
 
 lowerb = np.array([0, 0, 0])
 upperb = np.array([15, 15, 15])
