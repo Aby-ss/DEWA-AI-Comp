99721eeaede4c5ccb7aaca3b41526759a8c27509
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-03 13:42:40 +0400

    Main_cam Update #1

--- Junior Team/AI/Main_Cam.py
+++ Junior Team/AI/Main_Cam.py
@@ -1,10 +1,18 @@
 import numpy as np
 import cv2
-img = cv2.imread('Pictures/image.jpg')
+
+from rich.traceback import install
+install(show_locals=True)
+
+img = cv2.imread("Junior Team\\Pictures\\image.jpg", 1)
+
 blur = cv2.GaussianBlur(img, (7, 7), 2)
 h, w = img.shape[:2]
-# Morphological gradient
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
-cv2.imshow('Morphological gradient', gradient)
+
+# Binarize gradient
+
+lowerb = np.array([0, 0, 0])
+upperb = np.array([15, 15, 15])
+binary = cv2.inRange(gradient, lowerb, upperb)
+cv2.imshow('Binarized gradient', binary)
 cv2.waitKey()
