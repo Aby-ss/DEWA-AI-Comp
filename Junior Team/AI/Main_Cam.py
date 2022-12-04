b912b3c9e4914cb3e97592d94fbce1ef1ee38a3b
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-04 11:56:11 +0400

    Error pending #1

--- Junior Team/AI/Main_Cam.py
+++ Junior Team/AI/Main_Cam.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
 
+from rich.panel import Panel
+from rich import box
+
 from rich.traceback import install
 install(show_locals=True)
 
-img = cv2.imread("C:\\Users\\hadir\\Documents\\VSC - Projects\\DEWA-AI-Comp\\Junior Team\\Pictures\\pipe#2.jpg", 1)
+img = cv2.imread("C:\\Users\\hadir\\Documents\\VSC - Projects\\DEWA-AI-Comp\\Junior Team\\Pictures\\pipe#3.jpg", 1)
 
 blur = cv2.GaussianBlur(img, (7, 7), 2)
 h, w = img.shape[:2]
@@ -18,4 +21,11 @@ lowerb = np.array([0, 0, 0])
 upperb = np.array([15, 15, 15])
 binary = cv2.inRange(gradient, lowerb, upperb)
 cv2.imshow('Binarized gradient', binary)
-cv2.waitKey()
+
+image = binary
+k = cv2.waitKey(0)
+
+if( k == ord('g') ):
+  cv2.imwrite('Detected Image', image )
+  print(Panel("Image saved", box = box.SQUARE, border_style = "Bold Red"))
+  cv2.destroyAllWindows()
