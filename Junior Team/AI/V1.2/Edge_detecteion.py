b348ff71a89aa6bed207ba60fb6b8db424f802cd
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-09 20:53:33 +0400

    Junior Add

---
+++ Junior Team/AI/V1.2/Edge_detecteion.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+
+from rich.traceback import install
+install(show_locals = True)
+
+cam = cv.VideoCapture(0)
+
+while True:
+    _, frame = cam.read()
+    
+    cv.imshow("Camera Feed", frame)
+    
+    laplacian = cv.Laplacian(frame, cv.CV_64F)
+    laplacian = np.uint8(laplacian)
+    cv.imshow("Laplacian Effect", laplacian)
+    
+    edges = cv.Canny(frame, 100, 100)
+    cv.imshow("Edges Effect", edges)
+    
+    if cv.waitKey(5) == ord("q"):
+        break
+
+cam.release()
+cv.destroyAllWindows()
