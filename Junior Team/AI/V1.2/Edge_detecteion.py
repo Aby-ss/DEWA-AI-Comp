59a6241a71530ad360014065e956f30d08e0941b
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-10 10:17:44 +0400

    No laplacian Effect

--- Junior Team/AI/V1.2/Edge_detecteion.py
+++ Junior Team/AI/V1.2/Edge_detecteion.py
@@ -9,11 +9,11 @@ cam = cv.VideoCapture(0)
 while True:
     _, frame = cam.read()
     
-    cv.imshow("Camera Feed", frame)
-    
-    laplacian = cv.Laplacian(frame, cv.CV_64F)
-    laplacian = np.uint8(laplacian)
-    cv.imshow("Laplacian Effect", laplacian)
+    #cv.imshow("Camera Feed", frame)
+    #
+    #laplacian = cv.Laplacian(frame, cv.CV_64F)
+    #laplacian = np.uint8(laplacian)
+    #cv.imshow("Laplacian Effect", laplacian)
     
     edges = cv.Canny(frame, 100, 100)
     cv.imshow("Edges Effect", edges)
