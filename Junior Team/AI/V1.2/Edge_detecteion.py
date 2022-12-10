6f6951d9425c3ecb8886f4a097d71c9768d87a07
Rao <103417697+CodingAssassins@users.noreply.github.com>
2022-12-10 11:04:22 +0400

    Junior Intro

--- Junior Team/AI/V1.2/Edge_detecteion.py
+++ Junior Team/AI/V1.2/Edge_detecteion.py
@@ -4,22 +4,24 @@ import numpy as np
 from rich.traceback import install
 install(show_locals = True)
 
-cam = cv.VideoCapture(0)
 
-while True:
-    _, frame = cam.read()
-    
-    #cv.imshow("Camera Feed", frame)
-    #
-    #laplacian = cv.Laplacian(frame, cv.CV_64F)
-    #laplacian = np.uint8(laplacian)
-    #cv.imshow("Laplacian Effect", laplacian)
-    
-    edges = cv.Canny(frame, 100, 100)
-    cv.imshow("Edges Effect", edges)
-    
-    if cv.waitKey(5) == ord("q"):
-        break
+def edge_detect():
+    cam = cv.VideoCapture(0)
 
-cam.release()
-cv.destroyAllWindows()
+    while True:
+        _, frame = cam.read()
+        
+        #cv.imshow("Camera Feed", frame)
+        #
+        #laplacian = cv.Laplacian(frame, cv.CV_64F)
+        #laplacian = np.uint8(laplacian)
+        #cv.imshow("Laplacian Effect", laplacian)
+        
+        edges = cv.Canny(frame, 100, 100)
+        cv.imshow("Edges Effect", edges)
+        
+        if cv.waitKey(5) == ord("q"):
+            break
+
+    cam.release()
+    cv.destroyAllWindows()
---
+++ Junior Team/AI/V1.2/Intro.py
@@ -0,0 +1,44 @@
+from rich import box
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+from rich.progress import Progress
+from rich.prompt import Prompt, Confirm
+
+from time import sleep
+
+console = Console()
+
+
+print("""
+███████╗███╗   ███╗ █████╗ ███████╗██╗  ██╗     ██████╗██╗      █████╗ ███╗   ██╗
+██╔════╝████╗ ████║██╔══██╗██╔════╝██║  ██║    ██╔════╝██║     ██╔══██╗████╗  ██║
+███████╗██╔████╔██║███████║███████╗███████║    ██║     ██║     ███████║██╔██╗ ██║
+╚════██║██║╚██╔╝██║██╔══██║╚════██║██╔══██║    ██║     ██║     ██╔══██║██║╚██╗██║
+███████║██║ ╚═╝ ██║██║  ██║███████║██║  ██║    ╚██████╗███████╗██║  ██║██║ ╚████║
+╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝     ╚═════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝      
+""")
+
+print(Panel.fit("[b red]WELCOME[/] to the software presentation of [b blue]SMASH CLAN[/]. You will get familirized with the software and also get a \ntutorial on how to operate the system. \n       [b red]So lets get started !", title = ". . .", title_align = "left", subtitle = "SMASH CLAN", subtitle_align = "left"))
+
+
+is_rich_great = Confirm.ask("Start Tour?")
+assert is_rich_great
+
+print("""
+███████╗██████╗  ██████╗ ███████╗    ██████╗ ███████╗████████╗███████╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗
+██╔════╝██╔══██╗██╔════╝ ██╔════╝    ██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║
+█████╗  ██║  ██║██║  ███╗█████╗      ██║  ██║█████╗     ██║   █████╗  ██║        ██║   ██║██║   ██║██╔██╗ ██║
+██╔══╝  ██║  ██║██║   ██║██╔══╝      ██║  ██║██╔══╝     ██║   ██╔══╝  ██║        ██║   ██║██║   ██║██║╚██╗██║
+███████╗██████╔╝╚██████╔╝███████╗    ██████╔╝███████╗   ██║   ███████╗╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║
+╚══════╝╚═════╝  ╚═════╝ ╚══════╝    ╚═════╝ ╚══════╝   ╚═╝   ╚══════╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝      
+""")
+
+import Edge_detecteion as ed
+ed.edge_detect()
+
+print(Panel.fit("[b red]The Edge Detection[/] feature detects all the edges of any surface shown on the camera feed and processes them into a black and white video format so we can\neasily spot any uneven surfaces or cracks.\nThe system then saves a recording of this footage which can be analyzed by the staff.", title = "Explanation", title_align="left", box = box.SQUARE, subtitle=" ... "))
+
+sleep(15)
+
+
