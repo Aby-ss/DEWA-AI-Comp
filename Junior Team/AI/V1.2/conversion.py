a100feb70485f67bcc541e8b70247bf0a640169d
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-01-07 15:13:49 +0400

    Fixing Canny Issue

--- Junior Team/AI/V1.2/conversion.py
+++
@@ -1,8 +0,0 @@
-import moviepy
-import moviepy.editor as moviepy
-
-from rich.traceback import install
-install(show_locals=True)
-
-clip = moviepy.VideoFileClip("output.avi")
-clip.write_videofile("Vid.mp4")
