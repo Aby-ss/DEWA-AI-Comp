e1fe0c4bc746523df8b6a9834c245e50c5029473
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-01-09 10:33:08 +0400

    Motor Code Fix #3

--- Senior Team/Hardwaree/Motor.py
+++ Senior Team/Hardwaree/Motor.py
@@ -1,72 +1,61 @@
 from time import sleep
-from guiozero import App, Text, PushButton
-from gpioizero import Robot, LED
+from guizero import App, Text, PushButton
+from gpiozero import Motor, LED
 
-motor = Robot(left=(4), right=(17))
+motor = Motor(17,18)
 motorSwitch = LED(27)
 
-app = App(title = "LOGIC GATE - - MOVEMENT", layout="grid", height=601, width=801)
-message = Text(app, text="Dual Motor Control", grid=[4,0])
+app = App(title="GUI Development", layout="grid", height=600, width=800)
+message = Text(app, text="Single Motor Control Interface", grid=[4,0])
 
 motorSpeedForward = 0
 motorSpeedBackward = 0
 
-def toggle():
-    if button0.text == "Start":
-        motorSwitch.on()
-        button0.text = "Stop"
+def toggleSwitch():
+    if button0.text=="Start":
+       motorSwitch.on()
+       button0.text="Stop"
     elif button0.text == "Stop":
-        motorSwitch.off()
-        button0.text = "Start"
+         motorSwitch.off()
+         button0.text = "Start"
 
-def Forward_increase():
+def forwardSpeedIncrease():
     global motorSpeedForward
-    motor.forward(speed = motorSpeedForward)
-    print("Increased motor speed (forward) Current speed = "+ str(motorSpeedForward))
-    
+    motor.forward(speed=motorSpeedForward)
+    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedForward))
     motorSpeedForward += 0.1
-    
     if motorSpeedForward >= 1:
         motorSpeedForward = 1
-        
-        
-        
-def Forward_decrese():
+
+def forwardSpeedReduce():
     global motorSpeedForward
-    motor.forward(speed = motorSpeedForward)
-    print("Decreased motor speed (forward) Current speed = "+ str(motorSpeedForward))
-    
+    motor.forward(speed=motorSpeedForward)
+    print("Reduce speed of motor forward. Current speed = "+ str(motorSpeedForward))
     motorSpeedForward -= 0.1
-    
-    if motorSpeedForward <= 1:
-        motorSpeedForward = 1
+    if motorSpeedForward <= 0:
+        motorSpeedForward = 0
 
-def backward_increase():
+def backwardSpeedIncrease():
     global motorSpeedBackward
-    motor.forward(speed = motorSpeedBackward)
-    print("Increased motor speed (backward) Current speed = "+ str(motorSpeedForward))
-    
-    motorSpeedForward += 0.1
-    
-    if motorSpeedForward >= 1:
-        motorSpeedForward = 1
-        
-def backward_decrease():
-    global motorSpeedBackward
-    motor.forward(speed = motorSpeedBackward)
-    print("Increased motor speed (backward) Current speed = "+ str(motorSpeedForward))
-    
-    motorSpeedForward -= 0.1
-    
-    if motorSpeedForward <= 1:
-        motorSpeedForward = 1
-        
-Text(app, "Motor", grid=[2, 1])
+    motor.forward(speed=motorSpeedBackward)
+    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedBackward))
+    motorSpeedBackward += 0.1
+    if motorSpeedBackward >= 1:
+        motorSpeedBackward = 1
 
-button0 = PushButton(app, command = toggle(), text="Start", width = 10, height = 3, grid = [2, 4])
-button1 = PushButton(app, command = Forward_decrese(), text = "Forward +", width = 10, height = 3, grid = [2, 3])
-button2 = PushButton(app, command = Forward_decrese(), text = "Forward -", width = 10, height = 3, grid = [2, 5])
-button3 = PushButton(app, command = backward_increase(), text = "Backward +", width = 10, height = 3, grid = [1, 4])
-button4 = PushButton(app, command = backward_decrease(), text = "Backward -", width = 10, height = 3, grid = [3, 4])
+def backwardSpeedReduce():
+    global motorSpeedBackward
+    motor.backward(speed=motorSpeedBackward)
+    print("Reduce speed of motor backward. Current speed = "+ str(motorSpeedBackward))
+    motorSpeedBackward -= 0.1
+    if motorSpeedBackward <= 0:
+        motorSpeedBackward = 0
+
+Text(app, "Motor",grid=[2,1])
+button0 = PushButton(app, command=toggleSwitch, text="Start", width=10,height=3, grid=[2,4])
+button1 = PushButton(app, command=forwardSpeedIncrease, text="Frwd Speed +", width=10,height=3, grid=[2,3])
+button2 = PushButton(app, command=backwardSpeedReduce, text="Bckwd Speed -", width=10,height=3, grid=[2,5])
+button3 = PushButton(app, command=backwardSpeedIncrease, text = "Bckwd Speed +", width=10,height=3, grid=[1,4])
+button4 = PushButton(app, command=forwardSpeedReduce, text="Frwd Speed -", width=10, height=3, grid=[3,4])
 
 app.display()
